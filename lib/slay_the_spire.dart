d446a9d413037553e91ffa542dede9d7fd75637b
Fenysk <minefenysk@gmail.com>
2024-07-25 09:08:55 +0200

    🍻 Experimenting

--- lib/slay_the_spire.dart
+++ lib/slay_the_spire.dart
@@ -1,5 +1,30 @@
+import 'dart:async';
+import 'player.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
 
 class SlayTheSpire extends FlameGame {
   SlayTheSpire({super.children});
+
+  @override
+  FutureOr<void> onLoad() {
+    super.onLoad();
+
+    world.add(Player(
+      position: Vector2(-50, -50),
+      radius: 50,
+      color: Colors.red,
+    ));
+
+    world.add(Player(
+      position: Vector2(100, 100),
+      radius: 30,
+      color: Colors.blue,
+    ));
+  }
+
+  @override
+  Color backgroundColor() {
+    return Colors.green;
+  }
 }
